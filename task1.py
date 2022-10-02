bfa33026eca3b499ed3d9f371a4697819519c608
recall0a0dream <114842470+recall0a0dream@users.noreply.github.com>
2022-10-02 11:24:25 +0800

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,21 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 import random
-list1=[57,97,72,13,66,36,35,26,21,13]
+list1=[57,97,72,13,66,36,35,26,21,13]# Optionally set a list of numbers.
 tuple1=tuple(list1)
 n=len(list1)
 
@@ -28,8 +12,8 @@ for i in range(n-1):
             list1[j]=a
 print(list1)
 
-# Use the dichotomy to find a predetermined number's index in the list.
-a=random.choice(list1) #Choose a number from the list randomly.
+# Use the dichotomy to find a predetermined number's index in the sorted list.
+a=random.choice(list1) # Choose a number from the list randomly.
 m=0
 while len(list1)>=1:
     n=len(list1)
@@ -41,12 +25,14 @@ while len(list1)>=1:
     else:
         list1=list1[int((n-1)/2)+1:]
         m=m+int((n-1)/2)+1
+# The number m can help us to find the correct index of a in the initial ordered list1.
 if n==1:   # At this time, the selected a is the unique one.
     n=n
 elif n==2: # At this time, the selected a is definitely the first one.
     if list1[1] == a:
         print("The rank of ", a, " is ", m + int((n - 1) / 2) + 1)
 else:  # That means n>=3, then the selected number definitely not at the end.
+# By the way, the location of the above "else" will be wrong if you merely press the "Enter" at the end of the last line. It's due to the rule of retract of Python.
     for i in range(int((n - 1) / 2) - 1, -1, -1):
         if list1[i] == a:
             print("The rank of ", a, " is ", m + int((n - 1) / 2) - (int((n - 1) / 2) - i))
